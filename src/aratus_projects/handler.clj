c5a12a8f7b3c3ebc547d506435c585e12651aeda
josmas <josmasflores@gmail.com>
2015-04-29 23:07:41 +0100

    configuration for jetty to run on Openshift

--- src/aratus_projects/handler.clj
+++ src/aratus_projects/handler.clj
@@ -1,7 +1,8 @@
 (ns aratus-projects.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.adapter.jetty :refer [run-jetty]]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
@@ -9,3 +10,8 @@
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+(defn -main [& args]
+  (let [ip (get (System/getenv) "OPENSHIFT_CLOJURE_HTTP_IP" "0.0.0.0")
+        port (Integer/parseInt (get (System/getenv) "OPENSHIFT_CLOJURE_HTTP_PORT" "8080"))]
+    (run-jetty app {:host ip :port port})))
